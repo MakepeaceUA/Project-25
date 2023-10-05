112f1b49461f268e02b5cd8ef650f2a6f8846cfc
Arsen Sevastyanov <130586920+MakepeaceUA@users.noreply.github.com>
2023-10-05 15:01:00 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+
+
+//Задание 1
+/*void Func(char* str) 
+{    
+  char* ptr;    
+  for (ptr = str; *ptr; ptr++)    
+  {   
+    if (*ptr == '.')        
+   {            
+     *ptr = '!';        
+   }    
+  }
+}
+int main()
+{
+  setlocale(LC_CTYPE, "Rus");
+  char list[] = "Пр.ве.,М.ир!";
+  cout << list << "\n";
+  Func(list);
+  cout << list;
+}*/
+
+
+//Задание 2
+/*void Func(char* str,char symb) 
+{    
+  char* ptr;   
+  int count = 0;
+  for (ptr = str; *ptr; ptr++)    
+  {   
+    if (*ptr == symb)        
+   {            
+     count++;    
+   }    
+  }
+  cout << count;
+}
+
+int main()
+{
+  char list[] = "Hello World!";
+  char symbol = 'l';
+  Func(list,symbol);
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
